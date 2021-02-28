fce458a1ba21f1835a9be221a367d12218173c81
lavsam-dev <79759117+lavsam-dev@users.noreply.github.com>
2021-02-28 19:41:52 +0400

    Delete src/main/java/ru/geekbrains directory

--- src/main/java/ru/geekbrains/lesson1_intro/First.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.lesson1_intro;
-
-public class First {
-    public static void main(String[] args) {
-        System.out.println("любая херня");
-    }
-}
--- src/main/java/ru/geekbrains/lesson1_intro/MainApp.java
+++
@@ -1,8 +0,0 @@
-package ru.geekbrains.lesson1_intro;
-
-public class MainApp {
-    public static void main(String[] args) {
-        System.out.println("Who is it? " + 045673);
-
-    }
-}
